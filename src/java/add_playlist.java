2a9cfde4581f94505d04678a9af66abc11d5a164
Daivik Purani <35226862+DAIIVIIK@users.noreply.github.com>
2020-05-23 18:41:26 +0530

    Update add_playlist.java

--- src/java/add_playlist.java
+++ src/java/add_playlist.java
@@ -15,10 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Vrutik
- */
 public class add_playlist extends HttpServlet {
 
     /**
